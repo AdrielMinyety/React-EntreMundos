f1f2f9c9108e4eaa579fdd2fa7a172c0c750ee08
unknown <AM3114779@AM3114779.c3edu.online>
2019-09-29 21:14:06 -0400

    New Proyect

---
+++ precache-manifest.c0491a40a9b66fb1ad013d3e35c30bd1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c71d99ed0e6b9f5363cf2136b67edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fcdbd6c354e221dd090",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bedb7784267caf67ed32",
+    "url": "/static/js/2.88a1ac9f.chunk.js"
+  },
+  {
+    "revision": "2fcdbd6c354e221dd090",
+    "url": "/static/js/main.1dcd74c6.chunk.js"
+  },
+  {
+    "revision": "94c9d5837ce7fee79350",
+    "url": "/static/js/runtime-main.13123234.js"
+  },
+  {
+    "revision": "55141710b398043b3e480ec8e857b4cb",
+    "url": "/static/media/agujeronegro.55141710.png"
+  },
+  {
+    "revision": "6b7c66102eb8f141d8b2dbf0f0d565f8",
+    "url": "/static/media/jupiter.6b7c6610.png"
+  },
+  {
+    "revision": "ab0189e1d9c1292cbc840ba315f04e20",
+    "url": "/static/media/luna.ab0189e1.png"
+  },
+  {
+    "revision": "5427a5ddd17eabdcf8972e35ac5bf2f6",
+    "url": "/static/media/marte.5427a5dd.png"
+  },
+  {
+    "revision": "c095a7aa404999c1f2c8ac32c9286b93",
+    "url": "/static/media/saturno.c095a7aa.png"
+  }
+]);
